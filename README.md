a01033ef3cff83706246edde66bddcaa8438c071
Jairo Grateron <jgrateron@jgmail.com>
2022-06-11 19:29:04 -0400

    cambiar color qr

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # qr-java
+
+# Requisitos
+
+* Java 11 o superior
+* maven
+
+# Ejecución
+
+* mvn package
+* java -jar target/qr-0.0.1-SNAPSHOT.jar
--- src/main/java/com/fresco/App.java
+++ src/main/java/com/fresco/App.java
@@ -1,5 +1,6 @@
 package com.fresco;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.Map;
@@ -8,6 +9,7 @@ import javax.imageio.ImageIO;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
+import com.google.zxing.client.j2se.MatrixToImageConfig;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
@@ -17,21 +19,22 @@ public class App {
 	public static void main(String[] args) {
 
 		try {
-			ImageIO.write(generateQRCodeImage("https://twitter.com/jgrateron", 400, 400), "png",
-					new File("mitwitter.png"));
+			var qr = generateQRCodeImage("https://twitter.com/jgrateron", 450, 450, Color.BLUE, Color.WHITE);
+			ImageIO.write(qr, "png", new File("mitwitter.png"));
 
-			ImageIO.write(generateQRCodeImage("https://github.com/jgrateron/qr-java", 400, 400), "png",
-					new File("migithub.png"));
+			qr = generateQRCodeImage("https://github.com/jgrateron/qr-java", 450, 450, new Color(45, 87, 44), Color.WHITE);
+			ImageIO.write(qr, "png", new File("migithub.png"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	public static BufferedImage generateQRCodeImage(String barcodeText, int width, int height) throws Exception {
+	public static BufferedImage generateQRCodeImage(String barcodeText, int width, int height, Color onColor, Color offColor) throws Exception {
 		QRCodeWriter barcodeWriter = new QRCodeWriter();
 		BitMatrix bitMatrix = barcodeWriter.encode(barcodeText, BarcodeFormat.QR_CODE, width, height,
-				Map.of(EncodeHintType.MARGIN, "1"));
+				Map.of(EncodeHintType.MARGIN, "2"));
 
-		return MatrixToImageWriter.toBufferedImage(bitMatrix);
+		return MatrixToImageWriter.toBufferedImage(bitMatrix, new MatrixToImageConfig(onColor.getRGB(), offColor.getRGB()));
 	}
+
 }
